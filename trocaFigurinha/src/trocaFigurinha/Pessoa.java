14d1e52bfe9c5f456146282315ef8de483e1f6ab
aluno <aluno@DSN1251017055>
2022-04-13 08:07:58 -0300

    OPa

---
+++ trocaFigurinha/src/trocaFigurinha/Pessoa.java
@@ -0,0 +1,15 @@
+package trocaFigurinha;
+
+public class Pessoa {
+	String nome;
+	int numFigurinhas;
+	
+	void Receber(int numFigurinhas) {
+		this.numFigurinhas = this.numFigurinhas + numFigurinhas;
+		
+	}
+	
+	void Dar(int numFigurinhas) {
+		this.numFigurinhas = this.numFigurinhas - numFigurinhas;
+	}
+}
