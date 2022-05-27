b3102491abfff6eb24984a43d577c7686d011bc5
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 15:16:45 -0300

    Update Aplicacao.java

--- Sobrecarga/src/Aplicacao.java
+++ Sobrecarga/src/Aplicacao.java
@@ -3,27 +3,32 @@ import java.util.Scanner;
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		
+		// O metodo Scanner e usado para interagir com o usuario e permitir que ele possa colocar suas proprias respostas no console ao inves de so pegar
+		// respostas pre-determinadas
 		Scanner valor = new Scanner(System.in);
+		// Aqui foi instanciada a variavel matematica do tipo Matematica para poder ter acesso ao metodos da classe Matematica
 		Matematica matematica = new Matematica();
 		
 		
-		System.out.println("¡rea dos polÌgonos");
-		System.out.println("Digite a altura do polÌgono");
+		System.out.println("√Årea dos pol√≠gonos");
+		System.out.println("Digite a altura do pol√≠gono");
+		// O comando nextInt() e do metodo Scanner e permite que o usuario digite um valor e ele interprete como int
 		int x = valor.nextInt();
-		System.out.println("Digite a largura do polÌgono");
+		System.out.println("Digite a largura do pol√≠gono");
 		int y = valor.nextInt();
 		
+		// Aqui foram chamados os metodos da classe Matematica
 		int areaQ = matematica.area(x,y);
 		
 		double areaT = matematica.area(x,y);
 		
 		int areaR = matematica.area(x,y);
 		
+		// Resultado final sendo exibido para o usuario
 		System.out.println("");
-		System.out.println("A ·rea do quadrado È: " + areaQ);
-		System.out.println("A ·rea do tri‚ngulo È: " + areaT);
-		System.out.println("A ·rea do ret‚ngulo È: " + areaR);
+		System.out.println("A √°rea do quadrado √©: " + areaQ);
+		System.out.println("A √°rea do tri√¢ngulo √©: " + areaT);
+		System.out.println("A √°rea do ret√¢ngulo √©: " + areaR);
 	}
 
 }
