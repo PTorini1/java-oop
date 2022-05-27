00f8da2703636da91880a28f1da67f05ae532013
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 15:09:33 -0300

    Update Lampada.java

--- TrocarLampada/src/Lampada.java
+++ TrocarLampada/src/Lampada.java
@@ -2,19 +2,20 @@
 public class Lampada {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, Ol· Mundo, Hola Mundo");
+		//Projeto para testar o comando System.out.println
+		System.out.println("Hello World, Ol√° Mundo, Hola Mundo");
 		System.out.println("----------------------------------");
-		System.out.println("Pegar a l‚mpada");
+		System.out.println("Pegar a l√¢mpada");
 		System.out.println("Pegar a escada");
-		System.out.println("Deixar a l‚mpada em algum lugar prÛximo da que ser· trocada");
-		System.out.println("Colocar a escada em baixo da l‚mpada quebrada");
-		System.out.println("Remover a l‚mpada quebrada");
+		System.out.println("Deixar a l√¢mpada em algum lugar pr√≥ximo da que ser√° trocada");
+		System.out.println("Colocar a escada em baixo da l√¢mpada quebrada");
+		System.out.println("Remover a l√¢mpada quebrada");
 		System.out.println("Colocar em algum local seguro");
-		System.out.println("Pegar a l‚mpada nova");
+		System.out.println("Pegar a l√¢mpada nova");
 		System.out.println("Subir na escada novamente");
-		System.out.println("Colocar a l‚mpada nova");
+		System.out.println("Colocar a l√¢mpada nova");
 		System.out.println("Descer da escada");
-		System.out.println("Testar a l‚mpada");
+		System.out.println("Testar a l√¢mpada");
 
 	}
 
