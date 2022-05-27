fb1d08c929cb98ee4251953c3e9d993c116b468b
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 15:18:05 -0300

    Update Main.java

--- ProjetoTeste/src/ProjetoTeste/Main.java
+++ ProjetoTeste/src/ProjetoTeste/Main.java
@@ -2,6 +2,7 @@ package ProjetoTeste;
 
 public class Main {
 	public static void main(String[] args) {
+		// Aqui se deu o inicio do processo mais importante para se tornar um programador de respeito, printar Hello World na tela
 		System.out.println("Hello World");
 		// TODO Auto-generated method stub
 	}
