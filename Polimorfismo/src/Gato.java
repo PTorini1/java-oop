a23fc97d39582550ef83eff00e44143ea409aec7
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 17:57:15 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -1,6 +1,9 @@
-
+// Esse extends indica que a classe Gato tambem extende da classe animal
 public class Gato extends Animal{
-
+	
+	// O metodo falar do gato possui suas proprias caracteristicas que sao diferentes do cachorro
+	/* O cachorra fala com au au e gato com miau. Mesmo sendo o metodo com o mesmo nome e derivado da mesma classe, possui caracteristica propria pelo fato 
+	de o metodo da classe animal ser generico */ 
 	@Override
 	public void Falar() {
 		super.Falar();
