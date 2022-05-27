25562250d140beb20e16f8f06419398f90814de5
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 17:46:19 -0300

    Update Animal.java

--- Polimorfismo/src/Animal.java
+++ Polimorfismo/src/Animal.java
@@ -1,6 +1,8 @@
 
 public class Animal {
 	
+	// A classe animal possui um metodo generico que e o falar
+	// Esse metodo pode ser herdado por qualquer classe que herde de animal e possuir suas proprias caracteristicas
 	public void Falar() {
 		
 	}
