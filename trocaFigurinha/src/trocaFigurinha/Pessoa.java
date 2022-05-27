9ac0407aaff5e7abc166dbff9a55cf356427bf6d
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 15:07:18 -0300

    Update Pessoa.java

--- trocaFigurinha/src/trocaFigurinha/Pessoa.java
+++ trocaFigurinha/src/trocaFigurinha/Pessoa.java
@@ -1,18 +1,23 @@
 package trocaFigurinha;
 
 public class Pessoa {
+	// Criando atributos para a classe 
 	String nome;
 	int numFigurinhas;
+	// O atributo x serve para controlar a mensagem que serÃ¡ mostrada
 	int x=1;
 	
+	// Metodo que recebe as figurinhas
 	void Receber(int numFigurinhas) {
 		this.numFigurinhas += numFigurinhas;
 		
 	}
 	
-	void Dar(int numFigurinhas, Pessoa pessoa) {
+	// Metodo que da as figurinhas
+	void Dar(int numFigurinhas, Pessoa pessoa) {	
+		// Condicao if para impedir que o usuario dÃª mais figurinhas do que possui
 		if(this.numFigurinhas < numFigurinhas) {
-			System.out.println("Você não pode trocar, pois não possui a quantidade necessária de figurinhas \n");
+			System.out.println("VocÃª nÃ£o pode trocar, pois nÃ£o possui a quantidade necessÃ¡ria de figurinhas \n");
 			x=2;
 		}else {
 		this.numFigurinhas -= numFigurinhas;
