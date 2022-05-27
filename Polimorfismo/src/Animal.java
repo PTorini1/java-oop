d0584a6250d382d959b9f26b16f7fe807dd773d6
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 18:09:50 -0300

    Update Animal.java

--- Polimorfismo/src/Animal.java
+++ Polimorfismo/src/Animal.java
@@ -1,6 +1,6 @@
-// Animal e uma classe abstrata, portanto, generica.
-// Ser generica indica que ela possui metodos ou atributos que vao ser herdados por todas as outras herdeiras dela
-// Por ser abstrata nao pode ser instanciada. Apenas as classes que extenddem de Animal podem ser seus metodos
+// Animal e uma classe pai, portanto, seus filhos vao herdar caracteristicas dela.
+// Animal e uma classe generica. Ser generica indica que ela possui metodos ou atributos que vao ser herdados por todas as outras herdeiras dela e esses metodos e 
+// atributos nao possuem caracteristicas proprias. As caracteristicas serao definidas pelas classes herdeiras
 public class Animal {
 	
 	// A classe animal possui um metodo generico que e o falar
