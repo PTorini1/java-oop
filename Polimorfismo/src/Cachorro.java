c5e674e81fbd240ecbd9082469ca286747aaa121
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 17:49:32 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,12 +1,15 @@
-
+// Exntends inidica que a classe cachorro extende de animal e vai possuir os mesmos metodos que ela
 public class Cachorro  extends Animal{
-		
+	
+	// O override aqui foi usado para sobrescrever os metodos da classe animal na classe cachorro
 	@Override
 	public void Falar() {
+		// O super e necessario para indicar que o metodo deriva da classe abstrata
 		super.Falar();
 		System.out.println("AU AU");
 	}
 	
+	// Moder e um metodo presente apenas em cachorro e nao pode ser acessado por outros animais
 	public void Morder() {
 		System.out.println("NHAC!!");
 	}
