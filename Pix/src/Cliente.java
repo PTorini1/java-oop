3fce5b37e24aac6d88fa9de7fe8047b98ba901d7
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 18:06:37 -0300

    Delete Pix directory

--- Pix/src/Cliente.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Cliente {
-	String cpf = "111.222.333-44";
-	String telefone = "11999887766";
-	String email = "pedrotorini123@gmail.com";
-	String chaveAl = "abcd-1234";
-	float saldo = 2000;
-}
