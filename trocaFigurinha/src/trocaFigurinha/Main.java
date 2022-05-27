10bd862d99e71d769f4a931e8281c94848b3bcf2
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 15:07:14 -0300

    Update Main.java

--- trocaFigurinha/src/trocaFigurinha/Main.java
+++ trocaFigurinha/src/trocaFigurinha/Main.java
@@ -9,25 +9,24 @@ public class Main {
 		pessoa1.nome = "Pedro";
 		pessoa1.numFigurinhas = 20;
 		pessoa1.x = 1;
-		System.out.println("Número de figurinhas iniciais do Pedro: " + pessoa1.numFigurinhas);
+		System.out.println("NÃºmero de figurinhas iniciais do Pedro: " + pessoa1.numFigurinhas);
 
 		// Editando o perfil Jorge
 		Pessoa pessoa2 = new Pessoa();
 		pessoa2.nome = "Jorge";
 		pessoa2.numFigurinhas = 20;
-		System.out.println("Número de figurinhas iniciais do Jorge: " + pessoa2.numFigurinhas + "\n");
+		System.out.println("NÃºmero de figurinhas iniciais do Jorge: " + pessoa2.numFigurinhas + "\n");
 
 		// Realizando a troca
-
 		pessoa1.Dar(35, pessoa2);
 
 		// Resultado final
-
+		// CondiÃ§Ã£o para exibir a mensagem do resultado apenas se a variavel x for igual a 1 conforme indicado na Classe Pessoa
 		if (pessoa1.x > 1) {
 			System.out.println("");
 		} else {
-			System.out.println("Número de figurinhas do Pedro depois da troca: " + pessoa1.numFigurinhas);
-			System.out.println("Número de figurinhas do Jorge depois da troca: " + pessoa2.numFigurinhas);
+			System.out.println("NÃºmero de figurinhas do Pedro depois da troca: " + pessoa1.numFigurinhas);
+			System.out.println("NÃºmero de figurinhas do Jorge depois da troca: " + pessoa2.numFigurinhas);
 		}
 	}
 }
