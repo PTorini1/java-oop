a14806316d4c6f1f1a4f002aac0c1dcc2b9f1d26
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-28 20:59:54 -0300

    Update Conta.java

--- List/src/Conta.java
+++ List/src/Conta.java
@@ -1,5 +1,6 @@
 
 public class Conta {
+	// toString e um metodo do Java que vai retornar o valor das variaveis criadas e forma de texto
 	@Override
 	public String toString() {
 		return nome + " " + valor;
@@ -8,6 +9,7 @@ public class Conta {
 	String nome;
 	double valor;
 	
+	// Aqui foi instanciado o construtor da classe para que a classe principal pudesse acessar os atributos dessa
 	public Conta(String nome, double valor) {
 		this.nome = nome;
 		this.valor = valor;
