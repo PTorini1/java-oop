7995c77427e63d2b0232810673f760e393689efc
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-27 22:38:08 -0300

    Update Bebida.java

--- PolimorfismoBebidas/src/Bebida.java
+++ PolimorfismoBebidas/src/Bebida.java
@@ -1,18 +1,24 @@
 
 public class Bebida {
+	// Criando os atributos da bebida
 	private String nome;
 	private boolean aquecer;
 	
+	// Aqui e o metodo contrutor da classe bebida
+	// O contrutor e usado para que outras classes possam acessar os atributos privados da classe atual
 	public Bebida(String nome, boolean aquecer) {
 		super();
 		this.nome = nome;
 		this.aquecer = aquecer;
 	}
-
+	
+	// Metodo generico para qualquer bebida 
 	public void preparar() {
 		System.out.println("Pegando o copo");
 	}
 	
+	// Getters e setters dos atributos dessa classe
+	// Getters pegam o valor e setters atribuem um valor
 	public String getNome() {
 		return nome;
 	}
