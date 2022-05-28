ca91cf3f5efd01924675cb14d1a3cf685fa01ec3
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-28 19:40:48 -0300

    Update LeiteQuente.java

--- PolimorfismoBebidas/src/LeiteQuente.java
+++ PolimorfismoBebidas/src/LeiteQuente.java
@@ -1,10 +1,12 @@
 
 public class LeiteQuente extends Bebida{
 	
+	// O super serve para indicar que esta sendo puxado um metodo de outra classe. Nesse caso da Main
 	public LeiteQuente() {
 		super("Leite Quente", true);	
 	}
 
+	// Aqui foi pego um metodo generico da classe Bebida e adicionado mais um codigo especifico da classe LeiteQuente 
 	@Override
 	public void preparar() {
 		super.preparar();
