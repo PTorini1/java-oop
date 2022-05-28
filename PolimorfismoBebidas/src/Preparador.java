9f4a9831a423f06dc5c5fade32f07ed1668c43b1
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-28 20:46:27 -0300

    Update Preparador.java

--- PolimorfismoBebidas/src/Preparador.java
+++ PolimorfismoBebidas/src/Preparador.java
@@ -2,10 +2,12 @@
 public class Preparador{
 	
 	public void prepararBebida(Bebida bebida) {
+		// Metodo Generico para preparar bebida
 		System.out.println("Preparando Bebida " + bebida.getNome());
 		
 		bebida.preparar();
 		
+		// Metodo generico para aquecer, so funciona se for verdadeiro pois nao sera aplicado a todas as bebidas ao contrario do preparar
 		if(bebida.isAquecer() == true) {
 
 			System.out.println("Aquecendo a bebida " + bebida.getNome());
