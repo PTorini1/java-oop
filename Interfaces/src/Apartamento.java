6591d58b5ea6a3ecdd6f1baadf8c909a8e674603
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-28 21:01:29 -0300

    Update Apartamento.java

--- Interfaces/src/Apartamento.java
+++ Interfaces/src/Apartamento.java
@@ -1,6 +1,7 @@
 
 public class Apartamento implements ItemCaro {
-
+	// A classe apartamento faz parte de item caro
+	// Por isso herdou o metodo getPreco, mas possui seu valor especifico
 	@Override
 	public double getPreco() {
 		
