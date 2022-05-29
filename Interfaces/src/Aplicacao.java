68e779590fa2de8123d9638fac6cf7741adfb085
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-28 21:02:46 -0300

    Update Aplicacao.java

--- Interfaces/src/Aplicacao.java
+++ Interfaces/src/Aplicacao.java
@@ -2,20 +2,20 @@
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		
-//		Rota rota = new Rota();
+		// Classe principal para mostrar o que esta nas classes criadas, aqui so foram instanciados objetos para visualizacao dos conceitos de interface
+		Rota rota = new Rota();
 		Ferrari ferrari = new Ferrari();
-//		Fusca fusca = new Fusca();
-//		rota.ir(ferrari);
-//		rota.ir(fusca);
-//		System.out.println("---------------------");
-		//Automovel automovel = new Automovel();
+		Fusca fusca = new Fusca();
+		rota.ir(ferrari);
+		rota.ir(fusca);
+		System.out.println("---------------------");
+		Automovel automovel = new Automovel();
 		
-//		Automovel automovel = new Ferrari();
-//		rota.ir(automovel);
-//		automovel = new Fusca();
-//		rota.ir(automovel);
-//		System.out.println("---------------------");
+		Automovel automovel = new Ferrari();
+		rota.ir(automovel);
+		automovel = new Fusca();
+		rota.ir(automovel);
+		System.out.println("---------------------");
 	
 		Vendedor vendedor = new Vendedor();
 		Apartamento apartamento = new Apartamento();
