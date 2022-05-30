47c6c078e11125732e206983db0c7d5a752b3bb8
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-30 18:00:00 -0300

    Update Aplicacao.java

--- Interfaces/src/Aplicacao.java
+++ Interfaces/src/Aplicacao.java
@@ -6,17 +6,22 @@ public class Aplicacao {
 		Rota rota = new Rota();
 		Ferrari ferrari = new Ferrari();
 		Fusca fusca = new Fusca();
+		//Aqui foi chamado a variavel o metodo ir da classe rota que passa como parametro uma variavel do tipo ferrari
 		rota.ir(ferrari);
+		//Aqui foi chamado a variavel o metodo ir da classe rota que passa como parametro uma variavel do tipo fusca
 		rota.ir(fusca);
 		System.out.println("---------------------");
 		Automovel automovel = new Automovel();
 		
+		// Aqui nao foi necessario estanciar outra variavel automovel
+		// Como a classe Ferrari tambem implementa automovel, posso implementar ela direto
 		Automovel automovel = new Ferrari();
 		rota.ir(automovel);
 		automovel = new Fusca();
 		rota.ir(automovel);
 		System.out.println("---------------------");
-	
+		
+		// Instanciando objetos
 		Vendedor vendedor = new Vendedor();
 		Apartamento apartamento = new Apartamento();
 		
