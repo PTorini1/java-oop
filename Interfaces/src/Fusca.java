3882b631557ef5cbccc4bfae1a6ac96d0d15ec28
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-30 17:43:43 -0300

    Update Fusca.java

--- Interfaces/src/Fusca.java
+++ Interfaces/src/Fusca.java
@@ -1,6 +1,7 @@
-
+// A classe fusca implementou apenas a interface carro  e nao implementou a itemCaro, pois fusca nao e um item caro
 public class Fusca implements Carro {
 
+	// Os metodos virarEsquerda, direira e acelerar foram sobrescritos
 	@Override
 	public void virarEsquerda() {
 		System.out.println("Fusca virou a esquerda");
@@ -17,7 +18,7 @@ public class Fusca implements Carro {
 		System.out.println("Fusca acelerou");
 		
 	}
-
+	
 	@Override
 	public void abrirPorta() {
 		System.out.println("A porta foi aberta");
