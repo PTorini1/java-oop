72e13d31b722db8c3b5d9026de95ef11602c6213
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-30 17:38:21 -0300

    Update Ferrari.java

--- Interfaces/src/Ferrari.java
+++ Interfaces/src/Ferrari.java
@@ -1,6 +1,7 @@
-
+// Nessa caso foi necessario extender duas interfaces, pois a classe Ferrari possui metodos que serao usados das duas
 public class Ferrari implements Automovel, ItemCaro{
 
+	// O metodo da classe automovel foi sobrescrito para se adequar a Ferrari, isso aconteceu com os metodos virarEsquerda, virarDireita e acelerar
 	@Override
 	public void virarEsquerda() {
 		System.out.println("Ferrari virou a esquerda");
@@ -18,7 +19,8 @@ public class Ferrari implements Automovel, ItemCaro{
 		System.out.println("Ferrari acelerou");
 		
 	}
-
+	
+	// Aqui o metodo getPreco da classe ItemCaro foi sobrescrito
 	@Override
 	public double getPreco() {
 		return 1000000;
