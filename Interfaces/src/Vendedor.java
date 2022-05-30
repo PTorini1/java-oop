05716b97374d677000907fa76b7da97185e72fb2
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-30 17:49:50 -0300

    Update Vendedor.java

--- Interfaces/src/Vendedor.java
+++ Interfaces/src/Vendedor.java
@@ -1,8 +1,8 @@
 
 public class Vendedor {
-
+	// Aqui foi criada uma variavel mostrar preco que tem como parametro uma variavel caro do tipo ItemCaro e mostra o preco instanciado na interface ItemCaro
 	public void mostrarPreco(ItemCaro caro) {
-		System.out.println("O valor é " + caro.getPreco());
+		System.out.println("O valor Ã© " + caro.getPreco());
 	}
 	
 }
