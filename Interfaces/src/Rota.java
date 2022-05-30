f5dceefcbcf5ee6549f04d7a005b61b549d257fd
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-30 17:48:24 -0300

    Update Rota.java

--- Interfaces/src/Rota.java
+++ Interfaces/src/Rota.java
@@ -1,6 +1,6 @@
-
+// Classe para representar uma rota generica
 public class Rota {
-	
+	// Aqui foi criado um metodo que recebe como parametro uma variavel automovel do tipo automovel que chama os metodos daquela interface
 	public void ir(Automovel automovel){
 		
 		automovel.virarEsquerda();
