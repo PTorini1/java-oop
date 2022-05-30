eff2edddc1f40613ef28a0006fb67948e5d0f88e
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-30 17:26:46 -0300

    Update Carro.java

--- Interfaces/src/Carro.java
+++ Interfaces/src/Carro.java
@@ -1,6 +1,8 @@
-// se n„o escrever nada n„o d· erro porque È uma heranÁa
+// se n√£o escrever nada n√£o d√° erro porque √© uma heran√ßa
 public interface Carro extends Automovel{
 	
+	// Carro e um automovel, por isso extende de Automovel
+	// Mas como nem todo autmovel precisa abrir a porta, foi criado um metodo que e usado para carro e nao para todos os automoveis
 	public void abrirPorta();
 	
 	
