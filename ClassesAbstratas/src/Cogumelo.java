ec744c469c4d3bec5906cad06cb0802822f55675
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-05-30 18:12:35 -0300

    Update Cogumelo.java

--- ClassesAbstratas/src/Cogumelo.java
+++ ClassesAbstratas/src/Cogumelo.java
@@ -1,6 +1,6 @@
-
+// A classe cogumelo extende de Item, ou seja, possui os mesmos metodos que ela
 public class Cogumelo extends Item{
-
+	//Sobrescrevendo metodo pegar da classe abstrata item
 	@Override
 	public void pegar() {
 		System.out.println("Pegou Cogumelo");
