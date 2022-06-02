71a4d40810c3007ecca4a091ae699c1051b7d1a6
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-06-01 22:10:12 -0300

    Update Quadrado.java

--- ExercicioAreaCalculavel/src/Quadrado.java
+++ ExercicioAreaCalculavel/src/Quadrado.java
@@ -1,14 +1,17 @@
-
+// A classe quadrado implementa AreaCalculavel, pois os metodos dela se aplicam a essa
 public class Quadrado implements AreaCalculavel{
+	// Declarando variaveis
 	double x;
 	
+	// O metodo calcularArea foi sobrescrito a fim de passar um calculo que se aplica apenas a classe quadrado
 	@Override
 	public double calcularArea() {
 		return x *= x;
 	}
 
+	// Aqui e um metodo para imprimir a area na tela
 	public void mostrarArea() {
-		System.out.println("A área do quadrado é " + calcularArea() + " m2");
+		System.out.println("A Ã¡rea do quadrado Ã© " + calcularArea() + " m2");
 	}
 	
 }
