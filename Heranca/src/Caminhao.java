8d73e30c71449709ddee9908685bf6134305574e
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-06-01 22:43:59 -0300

    Update Caminhao.java

--- Heranca/src/Caminhao.java
+++ Heranca/src/Caminhao.java
@@ -1,6 +1,6 @@
-
+// exntends mostra que a classe Caminhao herda de veiculo
 public class Caminhao extends Veiculo{
-	
+	// Aqui foi sobrescrito o metodo buzinar, pois a buzina do caminhao e diferente do padrao definido
 	@Override
 	public void Buzinar() {
 		System.out.println("FON FON");
