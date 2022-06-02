cd327beb67ef3adfb8ddb2d69603d44764add640
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-06-01 22:11:02 -0300

    Update Retangulo.java

--- ExercicioAreaCalculavel/src/Retangulo.java
+++ ExercicioAreaCalculavel/src/Retangulo.java
@@ -1,15 +1,19 @@
-
+// A classe quadrado implementa AreaCalculavel, pois os metodos dela se aplicam a essa
 public class Retangulo implements AreaCalculavel{
+	// Declarando variaveis
 	double x;
 	double y;
 	
+	// O metodo calcularArea foi sobrescrito a fim de passar um calculo que se aplica apenas a classe quadrado
 	@Override
 	public double calcularArea() {
 		return x*y;
 	}
 
+	
+	// Aqui e um metodo para imprimir a area na tela
 	public void mostrarArea() {
-		System.out.println("A ·rea do Ret‚ngulo È " + calcularArea() + " m2");
+		System.out.println("A √°rea do Ret√¢ngulo √© " + calcularArea() + " m2");
 	}
 
 }
