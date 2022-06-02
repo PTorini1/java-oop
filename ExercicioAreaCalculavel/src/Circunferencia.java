d498f4acee992b414c4685b42627599a8e52c1b8
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-06-01 22:05:44 -0300

    Update Circunferencia.java

--- ExercicioAreaCalculavel/src/Circunferencia.java
+++ ExercicioAreaCalculavel/src/Circunferencia.java
@@ -1,5 +1,6 @@
 // A classe circunferencia implementa AreaCalculavel, pois os metodos dela se aplicam a essa
 public class Circunferencia implements AreaCalculavel{
+	// Declarando variaveis
 	double r;
 	
 	// O metodo calcularArea foi sobrescrito a fim de passar um calculo que se aplica apenas a classe cincunferencia
@@ -8,6 +9,7 @@ public class Circunferencia implements AreaCalculavel{
 		return r*r*Math.PI;
 	}
 
+	// Aqui e um metodo para imprimir a area na tela
 	public void mostrarArea() {
 		System.out.println("A área da Cinfureręncia é " + calcularArea() + " m2");
 	}
