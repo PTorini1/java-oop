23f76d8eb694eac83146f7bd80db0cd863624054
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-06-01 21:59:36 -0300

    Update Main.java

--- Enum/src/Main.java
+++ Enum/src/Main.java
@@ -10,18 +10,24 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		
+		// Instanciando uma variavel
 		double pi = Main.PI;
-
+		
+		// Instanciando objeto da classe DiaDaSemana e passa SEGUNDA como parametro
 		DiaDaSemana hoje = DiaDaSemana.SEGUNDA;
 		
+		// Instanciando variavel do tipo int cujo nome e num como um DiaDaSemana que vai o metodo getNum
+		// O getNum vai atribuir um numero o dia da semana, conforme feito na classe Enum
 		int num = DiaDaSemana.SEGUNDA.getNum();
 		System.out.println(num);
 		
+		// Um enum tambem pode ser chamado por String
 		String s = "TERCA";
+		// Aqui o objeto hoje, criado acima, e convertido para String a fim de receber o dia da semana
 		hoje = DiaDaSemana.valueOf(s);
 		System.out.println(hoje.getNum());
 		
+		// Aqui foi mostrado como e possivel chamar um Enum utilizando um objeto
 		Sexo sexo = Sexo.MASCULINO;
 		sexo = Sexo.FEMININO;
 		
