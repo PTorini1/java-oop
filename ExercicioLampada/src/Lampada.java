f5e91aca45ab2a1acceed679594c2a8ad6ec8bcc
aluno <aluno@DSN1251017055>
2022-06-06 11:04:57 -0300

    Aula 06/06

--- ExercicioLampada/src/Lampada.java
+++ ExercicioLampada/src/Lampada.java
@@ -1,19 +1,24 @@
 
 public class Lampada {
+	// variavel do tipo boolean que so recebe verdadeiro ou falso
 	private boolean ligada;	
-
+	
+	// Construtor 
 	public Lampada(boolean ligada) {
 		this.ligada = ligada;
 	}
 
+	// Metodo para ligar a lampada que atribui o boolean como true
 	public void ligar() {		
 		ligada = true;		
 	}
 	
+	// Metodo para ligar a lampada que atribui o boolean como false
 	public void desligar() {		
 		ligada = false;
 	}
 	
+	// Imprime o estado da lampada
 	public void imprimir() {
 		if(ligada == true) {
 			System.out.println("Ligada");
--- ExercicioLampada/src/LampadaMain.java
+++ ExercicioLampada/src/LampadaMain.java
@@ -4,23 +4,27 @@ public class LampadaMain {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-
+		
+		// 	criando variaveis
 		int criar = 0;
 		int estado = 0;
 
+		// Interacao com o cliente
 		System.out.println("Olá seja bem-vindo ao nosso sistema de criação de lâmpadas \n");
 		System.out.println("Pressione 1 para criar uma lâmpada");
 		criar = entrada.nextInt();
+		// Se quiser ele digitar algo nada a ver o sistema encerra
 		if (criar != 1) {
 			System.out.println("Obrigado por usar nosso sistema");
 
 		} else {
-
+			// Instanciando o objeto do tipo lampada e passando como para parametro sua booloean como true
 			Lampada lampada = new Lampada(true);
 
 			System.out.println("Quando a lâmpada é criada, ela já vem acesa");
 			System.out.println("Deseja mudar o estado da lâmpada? 1- Sim  2-Não\n");
 			estado = entrada.nextInt();
+			// if para imprimir o estado da lampada ou desligar de acordo com o que o usuario quiser
 			if (estado != 1) {
 				lampada.imprimir();
 			} else {
