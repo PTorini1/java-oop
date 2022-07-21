9853baa06e59de25a1aab6c7530c49610f47267e
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-07-20 21:47:41 -0300

    Update ProgramaRevisao2.java

--- Exercicios/src/fundamentos/ProgramaRevisao2.java
+++ Exercicios/src/fundamentos/ProgramaRevisao2.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class ProgramaRevisao2 {
 
 	public static void main(String[] args) {
+		float x=0;
+		float y=0;
 		Scanner entrada = new Scanner(System.in);
 		
-		List<Object> listaDados = new ArrayList<Object>();
+		List<DadosCadastro> listaDados = new ArrayList<DadosCadastro>();
 		for(int dados = 0; dados<=1; dados++ ){
 			DadosCadastro dadosCadastro = new DadosCadastro();
 			System.out.println("Alimentos Gostosos e Muito Bons LTDA");
@@ -19,16 +21,17 @@ public class ProgramaRevisao2 {
 			System.out.println("Digite seu salario");
 			dadosCadastro.setSalario(entrada.nextFloat());
 			listaDados.add(dadosCadastro);
-			if (dados == 1){
-				dadosCadastro.toString();
+			
+			if(dados == 0) {
+				x = dadosCadastro.getSalario();
+			}else {
+				y = dadosCadastro.getSalario();
 			}
 		}
-		
-		List<Object> listaDados2 = new ArrayList<Object>();
-		DadosCadastro dadosCadastro = new DadosCadastro();
-		for (Object dados : listaDados2) {
+
+		DadosCadastro dadosCadastro = new DadosCadastro();		
+		for (DadosCadastro dados : listaDados) {
 			System.out.println(dados);
 		}
+		System.out.println("A diferenca de salario entre os dois e de: " + (x-y));
 	}
-
-}
