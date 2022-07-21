83b0869be52ff43ea4d8ee477f1e2c800c645c7d
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2022-07-20 21:48:24 -0300

    Update DadosCadastro.java

--- Exercicios/src/fundamentos/DadosCadastro.java
+++ Exercicios/src/fundamentos/DadosCadastro.java
@@ -1,39 +1,35 @@
 package fundamentos;
 
 public class DadosCadastro {
-	private String nome;
-	private int cpf;
-	private float salario;
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getCpf() {
-		return cpf;
-	}
-	public void setCpf(int cpf) {
-		this.cpf = cpf;
-	}
-	public float getSalario() {
-		return salario;
-	}
-	public void setSalario(float salario) {
-		this.salario = salario;
-	}
-	public DadosCadastro() {
+		private String nome;
+		private int cpf;
+		private float salario;
 		
-	}
-	
-	public int diferencaSalario(){
-		return (int) (salario - salario);
-	}
-	
-	@Override
-	public String toString() {
-		return "A diferenca de salario entre " + getNome() + " e de " + diferencaSalario() + "]";
-	}
-	
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public int getCpf() {
+			return cpf;
+		}
+		public void setCpf(int cpf) {
+			this.cpf = cpf;
+		}
+		public float getSalario() {
+			return salario;
+		}
+		public void setSalario(float salario) {
+			this.salario = salario;
+		}
+		public DadosCadastro() {
+			
+		}
+		
+		@Override
+		public String toString() {
+			return "Nome: " + nome + " CPF: "+ cpf + " Salario " + salario;
+		}
+		
 }
